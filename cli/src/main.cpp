5a72d57fb98bb4a8669bb69936c011a9cf2c618f
Raisess <raisechif@gmail.com>
2022-11-05 13:20:35 -0300

    cli: some improvements

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -3,17 +3,13 @@
 #include <vector>
 #include <bitfunky/core/TorrentDownload.h>
 #include <bitfunky/core/TorrentSession.h>
+#include <bitfunky/util/Util.h>
 #include <cli-creator/Cli.h>
+#include "print.h"
 
-#define EXIT_KEY 'q'
-#define DEFAULT_OUTPUT "."
-#define ALIAS_MAX_PRINT_SIZE 30
+#define DEFAULT_OUTPUT_PATH() BF::Util::Env("HOME") + "/Downloads"
 
 using namespace CliCreator;
-
-void print_header();
-void print_torrent_info(const std::string& alias, const BF::TorrentDownloadState&);
-void print_footer();
 
 auto help = [](Arguments) {
   print_header();
@@ -24,32 +20,21 @@ auto help = [](Arguments) {
   return 0;
 };
 
+void download(const std::vector<std::shared_ptr<BF::TorrentDownload>>&);
+
 auto download_torrent = [](Arguments args) {
+  if (args.size() == 0) {
+    std::cerr << "Invalid argument, no file provided." << std::endl;
+    return 1;
+  }
+
+  std::vector<std::shared_ptr<BF::TorrentDownload>> torrents = {};
+  for (auto arg : args) {
+    torrents.push_back(BF::TorrentDownload::Create(arg, arg, DEFAULT_OUTPUT_PATH()));
+  }
+
   try {
-    if (args.size() == 0) {
-      std::cerr << "Invalid argument, no file provided." << std::endl;
-      return 1;
-    }
-
-    std::vector<std::shared_ptr<BF::TorrentDownload>> torrents = {};
-    for (auto arg : args) {
-      torrents.push_back(BF::TorrentDownload::Create(arg, arg, DEFAULT_OUTPUT));
-    }
-
-    BF::TorrentSession session;
-    session.push_download(torrents);
-
-    session.loop([torrents]() {
-      std::system("clear");
-      print_header();
-
-      for (auto torrent : torrents) {
-        print_torrent_info(torrent->get_alias(), torrent->state);
-      }
-
-      print_footer();
-    });
-
+    download(torrents);
     return 0;
   } catch (std::exception& err) {
     std::cerr << err.what() << std::endl;
@@ -68,32 +53,18 @@ int main(int argc, char* argv[]) {
   return cli.handle(argc, argv);
 }
 
-void print_header() {
-  std::cout << "-----> BitFunky CLI" << std::endl << std::endl;
-}
+void download(const std::vector<std::shared_ptr<BF::TorrentDownload>>& downloads) {
+  BF::TorrentSession session;
+  session.push_download(downloads);
 
-static std::string get_progress_bar(uint8_t progress) {
-  std::string progress_bar = "--------------------";
-  uint8_t chunk = progress / 5;
+  session.loop([downloads]() {
+    std::system("clear");
+    print_header();
 
-  for (int i = 0; i < chunk; i++) {
-    progress_bar.replace(i, 1, "#");
-  }
+    for (auto download : downloads) {
+      print_torrent_info(download->get_alias(), download->state);
+    }
 
-  return "[" + progress_bar + "]";
-}
-
-void print_torrent_info(const std::string& alias, const BF::TorrentDownloadState& state) {
-  std::cout << (alias.size() <= ALIAS_MAX_PRINT_SIZE ? alias : alias.substr(0, ALIAS_MAX_PRINT_SIZE) + "...");
-  std::cout << " ";
-  std::cout << get_progress_bar(state.progress);
-  std::cout << " " << unsigned(state.progress) << "%";
-  std::cout << " | " << unsigned(state.total_downloaded) << " kB";
-  std::cout << " | " << unsigned(state.download_rate) << " kB/s";
-  std::cout << " | " << "Status: " << state.status;
-  std::cout << " | " << "Peers: " << state.peers << std::endl;
-}
-
-void print_footer() {
-  std::cout << std::endl << "Thanks for using BitFunky @ Raisess" << std::endl;
+    print_footer();
+  });
 }
---
+++ cli/src/print.h
@@ -0,0 +1,33 @@
+#pragma once
+
+void print_header() {
+  std::cout << "-----> BitFunky CLI" << std::endl << std::endl;
+}
+
+static std::string get_progress_bar(uint8_t progress) {
+  std::string progress_bar = "--------------------";
+  uint8_t chunk = progress / 5;
+
+  for (int i = 0; i < chunk; i++) {
+    progress_bar.replace(i, 1, "#");
+  }
+
+  return "[" + progress_bar + "]";
+}
+
+#define ALIAS_MAX_PRINT_SIZE 30
+
+void print_torrent_info(const std::string& alias, const BF::TorrentDownloadState& state) {
+  std::cout << (alias.size() <= ALIAS_MAX_PRINT_SIZE ? alias : alias.substr(0, ALIAS_MAX_PRINT_SIZE) + "...");
+  std::cout << " ";
+  std::cout << get_progress_bar(state.progress);
+  std::cout << " " << unsigned(state.progress) << "%";
+  std::cout << " | " << unsigned(state.total_downloaded) << " kB";
+  std::cout << " | " << unsigned(state.download_rate) << " kB/s";
+  std::cout << " | " << "Status: " << state.status;
+  std::cout << " | " << "Peers: " << state.peers << std::endl;
+}
+
+void print_footer() {
+  std::cout << std::endl << "Thanks for using BitFunky @ Raisess" << std::endl;
+}
